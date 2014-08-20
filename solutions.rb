30cffadb7e4542e7b814f6b43eeb021a2a634b0d
Max Martin <max@puppetlabs.com>
2014-08-20 15:06:15 -0700

    More ruby solutions? Apparently did a long time ago

--- solutions.rb
+++ solutions.rb
@@ -120,6 +120,26 @@ def problem10
   sum
 end
 
+class Fixnum
+  def num_divisors
+    sum=0
+    (1..self).each do |x|
+      sum += 1 if self % x==0
+    end
+    sum
+  end
+end
+
+def problem12
+  triangle_base=1
+  curr_triangle=1
+  answer = while true
+    break curr_triangle if curr_triangle.num_divisors > 500
+    triangle_base += 1
+    curr_triangle += triangle_base
+  end
+end
+
 def problem13
   numbers = [37107287533902102798797998220837590246510135740250,
   46376937677490009712648124896970078050417018260538,
@@ -226,6 +246,33 @@ def problem13
   sum.to_s.split(//).slice(0,10).inject(""){|s,x| s+=x}
 end
 
+class Fixnum
+  def collatz_length
+    length=0
+    curr=self
+    while curr!=1:
+      if curr.even?
+        curr = curr/2
+      else
+        curr = 3*curr+1
+      end
+      length += 1
+    end
+    length
+  end
+end
+
+def problem14
+  longest=[0,0]
+  (1..1_000_000).each do |x|
+    if x.collatz_length > longest[1]
+      longest[0] = x
+      longest[1] = x.collatz_length
+    end
+  end
+  longest[0]
+end
+
 def problem16
   (2**1000).to_s.split(//).each.inject(0){|s,x|s+x.to_i}
 end
@@ -238,6 +285,57 @@ end
 
 def problem20
   (100.factorial).to_s.split(//).each.inject(0){|s,x|s+x.to_i}
+end
+
+def problem11
+  data = <<BIGNUM
+08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
+BIGNUM
+  data = data.split(/ |\n/)
+  grid = []
+  (0..20).each do |i|
+    grid << data[i...(i+20)]
+  end
+  grid.each { |a| a.map! {|x| x.to_i } }
+  greatest = 0
+  (0..19).each do |i|
+    (0..19).each do |j|
+      prods = []
+      #Horizontal number
+      prods << grid[i][j] * grid[i+1][j] * grid[i+2][j] * grid[i+3][j] if i<17
+      # vertical number
+      prods << grid[i][j] * grid[i][j+1] * grid[i][j+2] * grid[i][j+3] if j<17
+      # diagonal number
+      prods << grid[i][j] * grid[i+1][j+1] * grid[i+2][j+2] * grid[i+3][j+3] if i<17 && j<17
+      prods.each { |x| greatest=x if x>greatest}
+    end
+  end
+ #19.downto(4).each do |i|
+ #  19.downto(4).each do |j|
+ #    prod = grid[i][j] * grid[i-1][j-1] * grid[i-2][j-2] * grid[i-3][j-3]
+ #    greatest = prod if prod > greatest
+ #  end
+ #end
+  greatest
 end
 
 problem_runner
